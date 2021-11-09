ca57ed8fcbb5bcf508e4eb1f72ed20b16d61b533
thanh97 <nguyenthanhchansol1997@gmail.com>
2021-11-10 01:01:55 +0700

    First python app

--- app/decrypt.py
+++
@@ -1,7 +0,0 @@
-import sys
-import getpass
-import project_aes
-
-password = getpass.getpass('password> ')
-dec = project_aes.decrypt(sys.stdin.buffer.read(), password)
-sys.stdout.buffer.write(dec)
